a5032a1322eabc70c2daac6f230cf07e8f23382f
Stylianos Kamnakis <kamnis1997@gmail.com>
2020-07-04 18:45:30 +0300

    Added find weather of current location feature

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,32 @@ const searchInput = document.querySelector('input')
 const messageOne = document.querySelector('#messageOne')
 const messageTwo = document.querySelector('#messageTwo')
 
+const $locationButton = document.querySelector('#location-btn')
+
+$locationButton.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    if (!navigator.geolocation) {
+        return alert('Your browser does not support geolocation.')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        messageOne.textContent = 'Loading...'
+        messageTwo.textContent = ''
+
+        fetch(`/location?longitude=${position.coords.longitude}&latitude=${position.coords.latitude}`).then((response) => {
+            response.json().then((data) => {
+                if (data.error) {
+                    messageOne.textContent = data.error
+                } else {
+                    messageOne.textContent = data.location
+                    messageTwo.textContent = data.forecast
+                }
+            })
+        })
+    })
+})
+
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.get('/help', (req, res) => {
 
 app.get('/weather', (req, res) => {
     if (!req.query.address) {
-        return res.send({ error : 'You must provide an address' })
+        return res.send({ error: 'You must provide an address' })
     }
 
     geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
@@ -64,6 +64,22 @@ app.get('/weather', (req, res) => {
                 })
             })
         }
+    })
+})
+
+app.get('/location', (req, res) => {
+    if (!req.query.longitude || !req.query.latitude) {
+        return res.send({ error: 'You must provide coordinates' })
+    }
+
+    forecast(req.query.latitude, req.query.longitude, (error, forecastData) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        res.send({
+            forecast: forecastData
+        })
     })
 })
 
